fa54254cfc0ecb2b426cbd345ed54035696c44d5
andhera <56818861+andhera@users.noreply.github.com>
2019-10-21 13:48:04 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -20,13 +20,13 @@ from tg_bot.modules.helper_funcs.misc import paginate_modules
 PM_START_TEXT = """
 नमस्ते {}, मेरा नाम {} है! मैं यहां आपके ग्रूप को मैनज करने में आपकी सहायता करने के लिए हूं!.अगर आपके पास मुझे इस्तेमाल करने का कोई प्रश्न है तो /help पर क्लिक करे
 
-मैं ग्रुप मैनेजर बॉट हूं,मेरे [भगवान](tg://user?id={}) ये है,मेरे भविष्य के अपडेट इस चैनल में डाले जाएंगे - @MarieChechi & और मेरा सपोर्ट ग्रुप @CuratorCrew है!
+मैं ग्रुप मैनेजर बॉट हूं,मेरे [भगवान](tg://user?id={}) ये है,मेरे भविष्य के अपडेट इस चैनल में डाले जाएंगे - @bksinghtech & और मेरा सपोर्ट ग्रुप @AndheraOfficial है!
 
-यह मेरा [डिप्लॉय कोड है](https://heroku.com/deploy?template=https://github.com/TheDarkW3b/Marie-Hindi), आप मेरी तरह ही क्लोन बना सकते हैं।
+मुझे follow करे Twitter Facebook Instagram पर @bksinghtech @YourXOfficial
 
 अधिक कमांड के लिए /help पर क्लिक करें ...
 
-Keep in mind that any changes you DO do to the source have to be on github, as per the license.
+Please Join Our Group @AndheraOfficial अंधेरा कायम रहे ।
 """ 
 
 HELP_STRINGS = """
